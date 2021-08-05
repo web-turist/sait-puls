c850646b25779248ed46ad14e199ac72660be90b
Maxim <x.maxim84@mail.ru>
2021-08-05 23:16:18 +0300

    добавлен слайдер

---
+++ SRC/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/arrow_right.png"></button>'
+    });
+  });
