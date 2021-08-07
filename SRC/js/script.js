3bc8e44b095e20d1da022b4ca5c017f8bf364afc
Maxim <x.maxim84@mail.ru>
2021-08-07 21:29:26 +0300

    завершен блок с табами

--- SRC/js/script.js
+++ SRC/js/script.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/arrow_right.png"></button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/arrow_right.png"></button>'
+    });
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
   });
+
+  
