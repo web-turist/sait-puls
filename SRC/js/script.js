8de742a45d9be508b67c02443b90c579df988695
Maxim <x.maxim84@mail.ru>
2021-08-29 15:39:20 +0300

    созданы модальные окна, а так же прописаны скрипты к ним

--- SRC/js/script.js
+++ SRC/js/script.js
@@ -24,8 +24,25 @@ $(document).ready(function(){
         e.preventDefault();
         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-      })
-    })
+      });
+    });
+
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function () {
+      $('.overlay, #consultation').fadeIn();
+    });
+    
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #thanks, #order').fadeOut();
+    });
+
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn();
+      });
+    });
   });
 
   
