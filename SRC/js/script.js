916220f50cd257ec1edf017fab95bfe21c521bb1
Maxim <x.maxim84@mail.ru>
2021-08-29 18:29:09 +0300

    валидация форм завершена

--- SRC/js/script.js
+++ SRC/js/script.js
@@ -43,6 +43,37 @@ $(document).ready(function(){
         $('.overlay, #order').fadeIn();
       });
     });
+
+    // Validate
+
+    function valideForms(form) {
+      $(form).validate({
+        rules: {
+          name: "required",
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          name: "Пожалуйста введите своё имя",
+          phone: "Пожалуйста введите свой номер телефона",
+          email: {
+            required: "Пожалуйста введите свой email",
+            email: "Неправлиьно введен email"
+          }
+        }
+      });
+    }
+
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
+
+    //Mask
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
   });
 
   
